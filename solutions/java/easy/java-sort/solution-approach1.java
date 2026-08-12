8d9610ade8c6ecfde0cf02b42924f447d6d8f523
anurudh-ui <anurudhparupalli@gmail.com>
2026-08-12 09:49:28 +0530

    ✅ Java Sort [java] — approach1

---
+++ solutions/java/easy/java-sort/solution-approach1.java
@@ -0,0 +1,65 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-sort/problem?isFullScreen=true
+// Problem     Java Sort
+// Difficulty  Easy
+// Subdomain   Data Structures
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-12, 09:49 a.m.
+// ──────────────────────────────────────────────────
+
+import java.util.*;
+class Student {
+    private int id;
+    private String fname;
+    private double cgpa;
+    public Student(int id, String fname, double cgpa) {
+        super();
+        this.id = id;
+        this.fname = fname;
+        this.cgpa = cgpa;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getFname() {
+        return fname;
+    }
+    public double getCgpa() {
+        return cgpa;
+    }
+}
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        List<Student> studentList = new ArrayList<Student>();
+        while (testCases > 0) {
+            int id = in.nextInt();
+            String fname = in.next();
+            double cgpa = in.nextDouble();
+            Student st = new Student(id, fname, cgpa);
+            studentList.add(st);
+            testCases--;
+        }
+        Collections.sort(studentList, new Comparator<Student>() {
+            public int compare(Student s1, Student s2) {
+                if (s1.getCgpa() != s2.getCgpa()) {
+                    return Double.compare(s2.getCgpa(), s1.getCgpa());
+                } 
+                else if (!s1.getFname().equals(s2.getFname())) {
+                    return s1.getFname().compareTo(s2.getFname());
+                } 
+                else {
+                    return Integer.compare(s1.getId(), s2.getId());
+                }
+            }
+        });
+        
+        for (Student st : studentList) {
+            System.out.println(st.getFname());
+        }
+        in.close();
+    }
+}
