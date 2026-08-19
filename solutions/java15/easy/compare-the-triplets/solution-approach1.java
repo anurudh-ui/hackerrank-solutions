6d7b99e69f4b1ab338fc6f5cd36a0af05ee38a45
anurudh-ui <anurudhparupalli@gmail.com>
2026-08-19 09:15:11 +0530

    ✅ Compare the Triplets [java15] — approach1

--- solutions/java15/easy/compare-the-triplets/solution-approach1.java
+++ solutions/java15/easy/compare-the-triplets/solution-approach1.java
@@ -6,7 +6,7 @@
 // Platform    HackerRank
 // Language    java15
 // Status      Accepted
-// Submitted   2026-08-19, 09:05 a.m.
+// Submitted   2026-08-19, 09:15 a.m.
 // ──────────────────────────────────────────────────
 
 import java.io.*;
